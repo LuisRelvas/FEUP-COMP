42e185edf84c3466b3b1719a4c8af1bd9b0dc38e
LuisRelvas <luisrelvas@netcabo.pt>
2024-04-13 15:54:49 +0100

    Fixing constructor structure

--- src/main/pt/up/fe/comp2024/optimization/OllirExprGeneratorVisitor.java
+++ src/main/pt/up/fe/comp2024/optimization/OllirExprGeneratorVisitor.java
@@ -203,32 +203,11 @@ public class OllirExprGeneratorVisitor extends PreorderJmmVisitor<Void, OllirExp
         var ollirType = "";
         var lhs = visit(node.getJmmChild(0));
         computation.append(lhs.getComputation());
-        var auxiliarType = TypeUtils.getExprType(node.getJmmChild(0),table);
-        var optionalParams = table.getParametersTry(currentMethod);
-        if(node.getJmmChild(0).getKind().equals(THIS_EXPR.toString()) || ((table.getMethods().contains(node.get("value"))) && auxiliarType.getName().equals(table.getClassName())))
+        if(node.getJmmChild(0).getKind().equals(THIS_EXPR.toString()) || table.getMethods().contains(node.get("value")))
         {
             returnType = table.getReturnType(node.get("value"));
             ollirType = OptUtils.toOllirType(returnType);
             typeFunction = "invokevirtual";
-        }
-
-        else if(optionalParams.isPresent())
-        {
-            var paramsCheck = optionalParams.get();
-            var type = TypeUtils.getExprType(node.getJmmChild(0),table);
-            for(Symbol s : paramsCheck)
-            {
-                if(s.getType().getName().equals(type.getName()))
-                {
-                    returnType = s.getType();
-                    ollirType = OptUtils.toOllirType(returnType);
-                    typeFunction = "invokevirtual";
-                }
-            }
-            if(typeFunction.isEmpty())
-            {
-                typeFunction = "invokestatic";
-            }
         }
         else
         {
@@ -236,17 +215,16 @@ public class OllirExprGeneratorVisitor extends PreorderJmmVisitor<Void, OllirExp
         }
         if(typeFunction.equals("invokevirtual")) {
             for (int i = 1; i < node.getNumChildren(); i++) {
-                    var rhs = visit(node.getJmmChild(i));
-                    computation.append(rhs.getComputation());
-                    params.append(rhs.getCode());
-                    if(i != node.getNumChildren() - 1)
-                    {
-                        params.append(",");
-                    }
+                var rhs = visit(node.getJmmChild(i));
+                computation.append(rhs.getComputation());
+                params.append(rhs.getCode());
+                if(i != node.getNumChildren() - 1)
+                {
+                    params.append(",");
+                }
 
                 hasArgs = true;
             }
-            if(node.getParent().getKind().equals(ASSIGN_STMT.toString()) || node.getParent().getKind().equals(BINARY_EXPR.toString())){
             var temp = OptUtils.getTemp();
             code = temp + ollirType;
             computation.append(code).append(SPACE).append(ASSIGN).append(ollirType).append(SPACE).append(typeFunction).append("(").append(lhs.getCode()).append(",").append("\"").append(node.get("value")).append("\"");
@@ -254,20 +232,12 @@ public class OllirExprGeneratorVisitor extends PreorderJmmVisitor<Void, OllirExp
             {
                 computation.append(",").append(params);
             }
-            computation.append(")").append(ollirType).append(END_STMT);}
-            else {
-                computation.append(typeFunction).append("(").append(lhs.getCode()).append(",").append("\"").append(node.get("value")).append("\"");
-                if(!params.isEmpty())
-                {
-                    computation.append(",").append(params);
-                }
-                computation.append(")").append(ollirType).append(END_STMT);
-            }
+            computation.append(")").append(ollirType).append(END_STMT);
         }
         else {
             for(int i = 1; i < node.getNumChildren();i++)
             {
-                if(node.getParent().getKind().equals(ASSIGN_STMT.toString()) || node.getParent().getKind().equals(BINARY_EXPR.toString()))
+                if(node.getParent().getKind().equals(ASSIGN_STMT.toString()))
                 {
                     var temp = OptUtils.getTemp();// get a new temp
                     ollirType = OptUtils.toOllirType(TypeUtils.getExprType(node.getParent(),table));
@@ -277,23 +247,24 @@ public class OllirExprGeneratorVisitor extends PreorderJmmVisitor<Void, OllirExp
                 }
                 var rhs = visit(node.getJmmChild(i));
                 computation.append(rhs.getComputation());
-                    params.append(rhs.getCode());
-                    if(i != node.getNumChildren() - 1)
-                    {
-                        params.append(",");
-                    }
+                params.append(rhs.getCode());
+                if(i != node.getNumChildren() - 1)
+                {
+                    params.append(",");
+                }
             }
             computation.append(typeFunction).append("(").append(node.getJmmChild(0).get("value")).append(",").append("\"").append(node.get("value")).append("\"");
             if(!params.isEmpty())
             {
                 computation.append(",").append(params);
             }
-                computation.append(")").append(".V").append(END_STMT);
+            computation.append(")").append(".V").append(END_STMT);
 
 
         }
         return new OllirExprResult(code,computation);
     }
+
 
     /**
      * Default visitor. Visits every child node and return an empty result.
