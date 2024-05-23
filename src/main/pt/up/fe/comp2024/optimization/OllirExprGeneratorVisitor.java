7bb0a4f44a7e1330893d1bae6b118168087cc438
LuisRelvas <luisrelvas@netcabo.pt>
2024-05-23 18:21:40 +0100

    varArgs methodCall can be empty

--- src/main/pt/up/fe/comp2024/optimization/OllirExprGeneratorVisitor.java
+++ src/main/pt/up/fe/comp2024/optimization/OllirExprGeneratorVisitor.java
@@ -396,40 +396,37 @@ public class OllirExprGeneratorVisitor extends AJmmVisitor<Void, OllirExprResult
                 //iterate over the parameters of the method
                 for(int i = 0; i < paramsAux.size(); i++) {
                     if (paramsAux.get(i).getType().isArray()) {
-                        if (!node.getJmmChild(i+1).getKind().equals(ARRAY_CREATION_EXPR.toString()) && !node.getJmmChild(i+1).getKind().equals(NEW_ARRAY_EXPR.toString()) && !node.getJmmChild(i+1).getKind().equals(VAR_REF.toString())) {
-                            indexVarArgsStart = i + 1;
-                            varArgs = true;
-                            var temp2 = OptUtils.getTemp();
-                            tempFixed = temp2;
-                            ollirType = OptUtils.toOllirType(paramsAux.get(i).getType());
-                            arraysCode.append(temp2).append(".array").append(ollirType).append(",");
-                            computation.append(temp2).append(".array").append(ollirType).append(ASSIGN).append(".array").append(ollirType).append(SPACE).append("new(array, ");
-                            //append the rest of the childs that are not varRef
-                            var total = -1;
-                            for(int m = 0; m < node.getNumChildren(); m++)
-                            {
-                                if(!node.getJmmChild(m).getKind().equals(VAR_REF.toString()))
-                                {
-                                    total++;
+                        if(node.getNumChildren() > 1) {
+                            if (!node.getJmmChild(i + 1).getKind().equals(ARRAY_CREATION_EXPR.toString()) && !node.getJmmChild(i + 1).getKind().equals(NEW_ARRAY_EXPR.toString()) && !node.getJmmChild(i + 1).getKind().equals(VAR_REF.toString())) {
+                                indexVarArgsStart = i + 1;
+                                varArgs = true;
+                                var temp2 = OptUtils.getTemp();
+                                tempFixed = temp2;
+                                ollirType = OptUtils.toOllirType(paramsAux.get(i).getType());
+                                arraysCode.append(temp2).append(".array").append(ollirType).append(",");
+                                computation.append(temp2).append(".array").append(ollirType).append(ASSIGN).append(".array").append(ollirType).append(SPACE).append("new(array, ");
+                                //append the rest of the childs that are not varRef
+                                var total = -1;
+                                for (int m = 0; m < node.getNumChildren(); m++) {
+                                    if (!node.getJmmChild(m).getKind().equals(VAR_REF.toString())) {
+                                        total++;
+                                    }
                                 }
-                            }
-                            if(node.getParent().getKind().equals(ARRAY_CREATION_EXPR.toString()) && node.getParent().getParent().getKind().equals(METHOD_CALL_EXPR.toString()))
-                            {
-                                total = total - 1;
-                            }
-                            computation.append(total).append(".i32 )");
-                            if(!ollirType.contains(".array"))
-                            {
-                                computation.append(".array");
-                            }
-                            computation.append(ollirType).append(END_STMT);
-                            for (int j = 0; j < node.getNumChildren() - 1 - i; j++) {
-                                var aux = visit(node.getJmmChild(j + 1 + i));
-                                if(node.getJmmChild(j+1+i).getKind().equals(ARRAY_ACCESS_EXPR.toString()))
-                                {
-                                    computation.append(aux.getComputation());
+                                if (node.getParent().getKind().equals(ARRAY_CREATION_EXPR.toString()) && node.getParent().getParent().getKind().equals(METHOD_CALL_EXPR.toString())) {
+                                    total = total - 1;
                                 }
-                                computation.append(temp2).append("[").append(j).append(".i32").append("]").append(ollirType).append(ASSIGN).append(ollirType).append(SPACE).append(aux.getCode()).append(END_STMT);
+                                computation.append(total).append(".i32 )");
+                                if (!ollirType.contains(".array")) {
+                                    computation.append(".array");
+                                }
+                                computation.append(ollirType).append(END_STMT);
+                                for (int j = 0; j < node.getNumChildren() - 1 - i; j++) {
+                                    var aux = visit(node.getJmmChild(j + 1 + i));
+                                    if (node.getJmmChild(j + 1 + i).getKind().equals(ARRAY_ACCESS_EXPR.toString())) {
+                                        computation.append(aux.getComputation());
+                                    }
+                                    computation.append(temp2).append("[").append(j).append(".i32").append("]").append(ollirType).append(ASSIGN).append(ollirType).append(SPACE).append(aux.getCode()).append(END_STMT);
+                                }
                             }
                         }
                     }
